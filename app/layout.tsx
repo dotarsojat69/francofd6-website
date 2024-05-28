351934c81c5bfa6c4d0f2cd91a8d3fbfaebe706a
dotarsojat69 <tdeath457@gmail.com>
2024-05-28 23:06:30 +0700

    feat: change title web

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Franco Porto | Personal Website",
+  title: "Franco Personal Website",
   description: "Personal Website",
 };
 
