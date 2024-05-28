baaadc199793cb67174ac038e20fa2244124a512
dotarsojat69 <tdeath457@gmail.com>
2024-05-28 23:33:16 +0700

    feat: add animation text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerate'
 import { FaLocationArrow } from 'react-icons/fa'
 import MagicButton from './ui/MagicButton'
+import TextEffect from './ui/TextEffect'
 
 const Hero = () => {
   return (
@@ -14,22 +15,23 @@ const Hero = () => {
       </div>
 
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.010] bg-grid-black/[0.02] flex items-center justify-center absolute top-0 left-0">
-      {/* Radial gradient for the container to give a faded look */}
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.09] bg-grid-black/[0.02] flex items-center justify-center absolute top-0 left-0">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
     </div>
 
     <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-            <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Franco Personal Website</h2>
+        <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Portofolio Web with Next.js
+          </p>
 
             <TextGenerateEffect
-                className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                words='Transforming Concepts into Seamless Experiences'
+                className='text-center text-[40px] md:text-4xl lg:text-6xl'
+                words='Transforming Ideas into Effortless User Experiences'
             />
 
             <p className='text-center md:tracking-wider mb-4 text-sm md-text-lg lg:text-2xl'>
-                Hi, I&apos;m Franco a Front End Developer
+                Hi, I&apos;m <TextEffect />
             </p>
 
             <a href="#about">
