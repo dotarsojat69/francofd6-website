5f25876b807a20dcdf51fb7a7988fa21afb52d3f
dotarsojat69 <tdeath457@gmail.com>
2024-06-12 00:15:31 +0700

    unused

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -1,52 +1,52 @@
-import React from "react";
-import Section from "./Section";
-import heroBackground from "../public/Hero-background.png";
-import TextEffect from './ui/TextEffect'
-import { Button } from "./ui/MovingBorders";
+// import React from "react";
+// import Section from "./Section";
+// import heroBackground from "../public/Hero-background.png";
+// import TextEffect from './ui/TextEffect'
+// import { Button } from "./ui/MovingBorders";
 
-const Hero2: React.FC = () => {
+// const Hero2: React.FC = () => {
 
-  return (
-    <Section
-      className="pt-[12rem] -mt-[5.25rem]"
-      crosses
-      crossesOffset="lg:translate-y-[5.25rem]"
-      customPaddings=''
-      id="about-me"
-    >
-      <div className="container relative">
-        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
-          <h1 className="h1 mb-6">
-            Hello,&nbsp;I&nbsp;Am{" "}
-            <span className="inline-block relative">
-              Tushar Nagar.
-            </span>
-          </h1>
-          <p className='text-center md:tracking-wider mb-4 text-sm md-text-lg lg:text-2xl'>
-            Hi, I&apos;m <TextEffect />
-          </p>
-          <Button href="https://x.com/Dev_FreAkeD" white>
-            Connect With Me
-          </Button>
-        </div>
-      </div>
-      <div className="container relative z-2 text-center">
-        <h1 className="h3 mb-6 inline-block relative">
-          <span className="inline-block relative">
-            About Me
+//   return (
+//     <Section
+//       className="pt-[12rem] -mt-[5.25rem]"
+//       crosses
+//       crossesOffset="lg:translate-y-[5.25rem]"
+//       customPaddings=''
+//       id="about-me"
+//     >
+//       <div className="container relative">
+//         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+//           <h1 className="h1 mb-6">
+//             Hello,&nbsp;I&nbsp;Am{" "}
+//             <span className="inline-block relative">
+//               Tushar Nagar.
+//             </span>
+//           </h1>
+//           <p className='text-center md:tracking-wider mb-4 text-sm md-text-lg lg:text-2xl'>
+//             Hi, I&apos;m <TextEffect />
+//           </p>
+//           <Button href="https://x.com/Dev_FreAkeD" white>
+//             Connect With Me
+//           </Button>
+//         </div>
+//       </div>
+//       <div className="container relative z-2 text-center">
+//         <h1 className="h3 mb-6 inline-block relative">
+//           <span className="inline-block relative">
+//             About Me
             
-          </span>
-          <p className="body-1 mx-50 pt-5 mb-6 text-n-2 lg:mb-6">
-            I'm <b>Tushar Nagar</b>, also known as <b>FreAkeD</b> in the gaming world. I began my coding journey in 2017, scripting for SA-MP servers.
-            As a front-end enthusiast, I'm currently expanding my skills to become a full-stack developer.
-            I love gaming and am passionate about exploring and growing in tech.
-          </p>
-        </h1>
-      </div>
-      <div className="absolute inset-0 w-auto" style={{ backgroundImage: `url(${heroBackground})`, backgroundSize: 'cover', backgroundPosition: 'center', filter: 'blur(2px)' }}></div>
-      <div className="absolute inset-0 bg-black opacity-50" />
-    </Section>
-  );
-};
+//           </span>
+//           <p className="body-1 mx-50 pt-5 mb-6 text-n-2 lg:mb-6">
+//             I'm <b>Tushar Nagar</b>, also known as <b>FreAkeD</b> in the gaming world. I began my coding journey in 2017, scripting for SA-MP servers.
+//             As a front-end enthusiast, I'm currently expanding my skills to become a full-stack developer.
+//             I love gaming and am passionate about exploring and growing in tech.
+//           </p>
+//         </h1>
+//       </div>
+//       <div className="absolute inset-0 w-auto" style={{ backgroundImage: `url(${heroBackground})`, backgroundSize: 'cover', backgroundPosition: 'center', filter: 'blur(2px)' }}></div>
+//       <div className="absolute inset-0 bg-black opacity-50" />
+//     </Section>
+//   );
+// };
 
-export default Hero2;
+// export default Hero2;
